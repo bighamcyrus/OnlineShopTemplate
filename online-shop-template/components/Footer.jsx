ace9581001c523a85b7f066d8e9448268f3bbf84
Cyrus Bigham <bighamcyrus@gmail.com>
2022-09-06 14:16:44 -0700

    product detail page looking good. Need to write conext logic 1:42

--- online-shop-template/components/Footer.jsx
+++ online-shop-template/components/Footer.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai';
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className="footer-container">
+      <p>2022 sneakerheads All rights reserverd</p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
     </div>
   )
 }
--- online-shop-template/components/Layout.jsx
+++ online-shop-template/components/Layout.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import Head from 'next/head';
 
-const Layout = () => {
+import NavBar from './NavBar';
+import Footer from './Footer';
+
+const Layout = ({ children }) => {
   return (
-    <div>Layout</div>
+    <div className="layout">
+      <Head>
+        <title>JS Mastery Store</title>
+      </Head>
+      <header>
+        <NavBar />
+      </header>
+      <main className="main-container">
+        {children}
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
   )
 }
 
